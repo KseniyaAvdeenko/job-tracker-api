c712d5192796c1332f16c8d6d62c056bf46775c1
Kseniya Avdeenko <105524305+KseniyaAvdeenko@users.noreply.github.com>
2024-11-12 22:39:17 +0300

    apy end point vacancy status

--- api/controllers/statusController.js
+++ api/controllers/statusController.js
@@ -3,10 +3,10 @@ const vacancyStatus = require('../models/vacancyStatus')
 
 const getVacancyStatus = (req, res) => {
     try {
-        res.status(200).join(vacancyStatus)
+        res.status(200).json(vacancyStatus)
     } catch (e) {
         handleError(res, 500, e.message)
     }
 }
 
-module.exports = {getVacancyStatus:getVacancyStatus}
+module.exports = {getVacancyStatus: getVacancyStatus}
