eab94022cfd042fc47f3d071cec378f0f4912fba
Kseniya Avdeenko <105524305+KseniyaAvdeenko@users.noreply.github.com>
2024-11-12 23:10:36 +0300

    apy end point vacancy status

--- api/models/vacancyStatus.js
+++ api/models/vacancyStatus.js
@@ -1,8 +1,10 @@
 const vacancyStatus = [
-     {status: 'Watched', name: 'Просмотрено', color: '#268E12'},
-     {status: 'Unwatched', name: 'Не просмотрено', color: '#C1CB09'},
-     {status: 'Rejected', name: 'Отказ', color: '#FF0000'},
-     {status: 'Archived', name: 'В архиве', color: '#656262'}
+    {status: 'Get test task', name: 'Получено тестовое', color: 'rgba(153,252,105,0.72)'},
+    {status: 'Invited to interview', name: 'Приглашен на собеседование', color: 'rgba(112,254,57,0.72)'},
+    {status: 'Get job offer', name: 'Получен оффер', color: 'rgba(72,249,4,0.72)'},
+    {status: 'Unwatched', name: 'Не просмотрено', color: '#C1CB09'},
+    {status: 'Rejected', name: 'Отказ', color: '#FF0000'},
+    {status: 'Archived', name: 'В архиве', color: '#656262'}
 ]
 
 module.exports = vacancyStatus;
