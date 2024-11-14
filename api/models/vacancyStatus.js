d96b9bd66dafb8863b53d4d649e06a1a514b5829
Kseniya Avdeenko <105524305+KseniyaAvdeenko@users.noreply.github.com>
2024-11-14 13:28:38 +0300

    update cors

--- api/models/vacancyStatus.js
+++ api/models/vacancyStatus.js
@@ -3,9 +3,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const vacancyStatusSchema = new Schema({
-    color: {type: String, required: false},
-    status: {type: String, required: true},
-    name: {type: String, required: false},
+    color: {type: String, required: true},
+    name: {type: String, required: true},
 })
 
 module.exports = mongoose.model('VacancyStatus', vacancyStatusSchema);
