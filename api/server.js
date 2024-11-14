6788c54c6742d1de796d3f339f28649985e84900
Kseniya Avdeenko <105524305+KseniyaAvdeenko@users.noreply.github.com>
2024-11-14 13:25:12 +0300

    update cors

--- api/server.js
+++ api/server.js
@@ -5,6 +5,7 @@ const vacanciesRoutes = require('./routes/vacanciesRoutes');
 const statusRoutes = require('./routes/statusRoutes')
 const mongoose = require('mongoose');
 require('dotenv').config()
+const cors = require('cors');
 
 
 mongoose.connect(process.env.MONGO_URL)
@@ -20,6 +21,11 @@ app.listen(PORT, (error) => {
 
 //middleware
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+app.use(cors({
+  origin: [process.env.REACT_URL_LOCAL, process.env.REACT_URL],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
+
 app.use(express.json())
 app.use(authRoutes)
 app.use(vacanciesRoutes)
